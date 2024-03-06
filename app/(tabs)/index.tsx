47dc36c59b11c7b0ac4ca8ad559676d86564e5d9
Pavan Kumar <pavankumar.palle@sparity.com>
2024-03-05 19:11:22 -0800

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,35 @@
-import { StyleSheet } from 'react-native';
-
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import Head from "expo-router/head";
+import { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
 
 export default function TabOneScreen() {
+  const [title, setTitle] = useState("Home")
+
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTitle("Pavan Title")
+    }, 1000)
+  }, [])
+
+
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content="Pet's Delight"></meta>
+        <meta name="keywords" content="Home"></meta>
+
+      </Head>
+      <View style={styles.container}>
+        <Text style={styles.title}>Tab One</Text>
+        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+        <EditScreenInfo path="app/(tabs)/index.tsx" />
+      </View>
+    </>
   );
 }
 
--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -2,14 +2,23 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import Head from 'expo-router/head';
 
 export default function TabTwoScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
-    </View>
+    <>
+      <Head>
+        <title>Second</title>
+        <meta name="description" content="Pet's Delight"></meta>
+        <meta name="keywords" content="second"></meta>
+
+      </Head>
+      <View style={styles.container}>
+        <Text style={styles.title}>Tab Two</Text>
+        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+        <EditScreenInfo path="app/(tabs)/two.tsx" />
+      </View>
+    </>
   );
 }
 
